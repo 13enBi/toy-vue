7d4c6ff23ed7fbb564b5c505262154a2d93d3e99
13enBi <2210576534@qq.com>
2021-04-21 00:20:41 +0800

    ref

--- src/reactive/reactive.ts
+++ src/reactive/reactive.ts
@@ -29,3 +29,29 @@ export const reactive = <T extends object>(source: T): T => {
 export const toRaw = <T>(source: T): T => {
 	return (source && (source as any)[RAW]) || source;
 };
+
+const REF = Symbol();
+
+export type Ref<T> = { value: T };
+
+export const ref = <T>(raw: T): Ref<T> => {
+	return {
+		[REF]: true,
+		[RAW]: raw,
+		_value: raw,
+
+		get value() {
+			track(this, '_value');
+
+			return this._value;
+		},
+		set value(val) {
+			this._value = val;
+			trigger(this, '_value');
+		},
+	} as any;
+};
+
+export const isRef = (val: any): val is Ref<any> => val && val[REF];
+
+export const unRef = <T>(val: T): T extends Ref<infer R> ? R : T => (isRef(val) ? val.value : val);
